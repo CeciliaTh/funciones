ab5e73624b174f5b20f8e591c324b70464eae4d9
Cecilia Thielemann <geminis2689@gmail.com>
2022-10-23 21:48:42 -0300

    Algunas modificaciones en los colores de los div

--- assets/js/divs.js
+++ assets/js/divs.js
@@ -5,9 +5,9 @@
             colorA.style.backgroundColor = "deeppink"
         } else if (event.key === 's'|| event.key === 'S') {
             var colorS = document.querySelector('#div2')
-            colorS.style.backgroundColor = "darkcyan"
+            colorS.style.backgroundColor = "purple"
         } else if (event.key === 'd' || event.key === 'D') {
             var colorD = document.querySelector('#div3')
-            colorD.style.backgroundColor = "lightgreen"
+            colorD.style.backgroundColor = "teal"
         }
     })
