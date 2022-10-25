e65c96af53fc56ed8073597121067b736f2f9628
Cecilia Thielemann <geminis2689@gmail.com>
2022-10-24 22:30:17 -0300

    Agregando index.html y otros detalles

--- assets/js/divs.js
+++ assets/js/divs.js
@@ -1,11 +1,13 @@
 let colorFondo = '';
     document.addEventListener('keydown', function (tecla) {
         if (tecla.key === 'a' || tecla.key === 'A') {
-           colorFondo = 'deeppink' 
+           colorFondo = 'red' 
         } else if (tecla.key === 's'|| tecla.key === 'S') {
             colorFondo = 'purple' 
         } else if (tecla.key === 'd' || tecla.key === 'D') {
             colorFondo = 'teal'
+        } else if (tecla.key === 'f' || tecla.key === 'F') {
+             colorFondo = 'greenyellow'
         } else{
             colorFondo = ''
         }
@@ -15,7 +17,9 @@ let colorFondo = '';
     let color1 = document.querySelector('#div1')
     let color2 = document.querySelector('#div2')
     let color3 = document.querySelector('#div3')
+    let color4 = document.querySelector('#div4')
 
     color1.addEventListener("click", function () { color1.style.backgroundColor = colorFondo });
     color2.addEventListener("click", function () { color2.style.backgroundColor = colorFondo });
     color3.addEventListener("click", function () { color3.style.backgroundColor = colorFondo });
+    color4.addEventListener("click", function () { color4.style.backgroundColor = colorFondo });
